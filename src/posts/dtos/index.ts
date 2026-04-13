4d715721aabd495a0afe11c88c9d1c9cf5787add
Ilina Tsanova <kniaginia.ili@gmail.com>
2026-04-13 13:34:20 +0300

    feat: posts pagination and filtering

--- src/posts/dtos/index.ts
+++ src/posts/dtos/index.ts
@@ -2,3 +2,4 @@ export * from './create-post.dto';
 export * from './post-response.dto';
 export * from './enhance-description.dto';
 export * from './comment-on-post.dto';
+export * from './posts-query.dto';
